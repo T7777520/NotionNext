c039fb0e4bc38c7963f28cd2f14cb376225023fa
T7777520 <hsdhak@myyahoo.com>
2026-03-16 18:49:29 +0800

    Update Footer.js

--- themes/matery/components/Footer.js
+++ themes/matery/components/Footer.js
@@ -15,7 +15,7 @@ import { siteConfig } from '@/lib/config'
 //   })()
 
   return (
-    <footer className='relative z-10 dark:bg-black flex-shrink-0 bg-indigo-700 text-gray-300 justify-center text-center m-auto w-full leading-6  dark:text-gray-100 text-sm p-6'>
+     <footer>
       // {/* <DarkModeButton/> */}
       // <i className='fas fa-copyright' /> {`${copyrightDate}`}{' '}
       // <span>
