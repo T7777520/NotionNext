ee1622c39cb40cb35c9f161823df904274d86d54
T7777520 <hsdhak@myyahoo.com>
2026-03-16 19:15:36 +0800

    Update Footer.js

--- themes/matery/components/Footer.js
+++ themes/matery/components/Footer.js
@@ -14,7 +14,7 @@ import { siteConfig } from '@/lib/config'
 //     return currentYear
 //   })()
 
-  return (
+  // return (
     // <footer className='relative z-10 dark:bg-black flex-shrink-0 bg-indigo-700 text-gray-300 justify-center text-center m-auto w-full leading-6  dark:text-gray-100 text-sm p-6'>
     //   {/* <DarkModeButton/> */}
     //   <i className='fas fa-copyright' /> {`${copyrightDate}`}{' '}
@@ -60,7 +60,7 @@ import { siteConfig } from '@/lib/config'
     //   </span>
     //   <br />
     // </footer>
-  )
-}
+  // )
+// }
 
-export default Footer
+// export default Footer
